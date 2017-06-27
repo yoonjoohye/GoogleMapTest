fa80545d1bef5dd3e24423b442f6d805bbcbafe3
yoonjoohye <67yjh00@gmail.com>
2017-06-27 18:52:10 +0900

    #4

--- app/src/main/java/a67yjh00/com/example/lg/googlemaptest/MainActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/googlemaptest/MainActivity.java
@@ -3,11 +3,28 @@ package a67yjh00.com.example.lg.googlemaptest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    GoogleMap googleMap;
+    SupportMapFragment mapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        this.googleMap=googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);//위성으로 되어있는 타입 위성 지도로 바꿈
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.83939,128.631),17));
+        googleMap.getUiSettings().setZoomControlsEnabled(true);//
     }
 }
